6e0160114d068f1f1d42899dfa0748b32fa5ccd5
Ahmd00z <ahmedmaged2004zsc@gmail.com>
2026-09-12 03:17:59 +0300

    add aiofiles library

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,5 +1,6 @@
 from .BaseController import BaseController 
 from fastapi import UploadFile
+from models import ResponseSignal 
 
 class DataController(BaseController): 
     def __init__(self):
@@ -7,10 +8,11 @@ class DataController(BaseController):
         self.size_scale = 1048576 # convert MB to bytes
 
     def validate_uploaded_file(self, file: UploadFile):
+
         if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
-            return False , "file not supported"
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
 
         if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:
-            return False  , "file exceed"
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
 
-        return True  , "sucsess"
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
