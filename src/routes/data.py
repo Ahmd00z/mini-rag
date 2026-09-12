198069bce2e0eb52c507be3d9161fbb17c3316ef
Ahmd00z <ahmedmaged2004zsc@gmail.com>
2026-09-12 03:17:59 +0300

    add aiofiles library

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends, UploadFile
-
-from controllers import DataController
-
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+import os
 from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
+import aiofiles
+from models import ResponseSignal
+import logging
 
 
 data_router = APIRouter(
@@ -21,6 +24,16 @@ async def upload_data(
 
     is_valid,result_signal = data_controller.validate_uploaded_file(file=file)
 
-    return {
-        "signal": result_signal
-    }
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
+
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path, file_id = data_controller.generate_unique_filepath(
+        orig_file_name=file.filename,
+        project_id=project_id
+    )
