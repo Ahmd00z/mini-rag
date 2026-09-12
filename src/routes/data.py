169bae9bb33d8f6b7af081636ce06c9dbb7d645e
Ahmd00z <ahmedmaged2004zsc@gmail.com>
2026-09-12 03:59:51 +0300

    add logging

--- src/routes/data.py
+++ src/routes/data.py
@@ -7,22 +7,22 @@ import aiofiles
 from models import ResponseSignal
 import logging
 
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1", "data"],
 )
 
-
 @data_router.post("/upload/{project_id}")
-async def upload_data(
-    project_id: str,
-    file: UploadFile,
-    app_settings: Settings = Depends(get_settings)
-):
+async def upload_data(project_id: str, file: UploadFile,
+                      app_settings: Settings = Depends(get_settings)):
+        
+    
+    # validate the file properties
     data_controller = DataController()
 
-    is_valid,result_signal = data_controller.validate_uploaded_file(file=file)
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
 
     if not is_valid:
         return JSONResponse(
@@ -37,3 +37,25 @@ async def upload_data(
         orig_file_name=file.filename,
         project_id=project_id
     )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
+
+    return JSONResponse(
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id": file_id
+            }
+        )
